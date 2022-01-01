ab3b38c15fbcc67f2129d118359810b3d65582f2
tanyagalkina <tatjana.galkin@epitech.eu>
2022-01-02 00:55:00 +0100

    [input feed] still fixing input feed

--- src/Errors.hs
+++ src/Errors.hs
@@ -47,10 +47,18 @@ direct_env_two (Env i) old = (0, SchQuote (fst $ head i), i)
 direct_env_two n      old  = (0, n, old)
 
 
+schedule :: [SchExpr] -> Ctx -> (Int, SchVal, Ctx)
+schedule (x:[]) ctx = direct_env (eval (x) ctx) ctx
+schedule (x:xs) ctx = schedule xs newctx
+  -- direct_env (eval (x) ctx) ctx
+                      where (a, b, newctx) =  direct_env ( eval x ctx) ctx
+
+
 evalSchTwo :: String -> Ctx -> (Int, SchVal, Ctx)
 evalSchTwo input  ctx | output == [] =  (84, (SchString "something went wrong"), ctx)
                    | snd (head output) /= "" = (84, (SchString "Did Not Parse Everything"), ctx)
-                   | True = (direct_env (eval (head (fst $ head output)) ctx) ctx)
+                   -- True = (direct_env (eval (head (fst $ head output)) ctx) ctx)
+                   | True = schedule (fst $ head output) ctx
                         -- where output = parse scheme input
                           where output =  parse atoms input
 
@@ -77,4 +85,4 @@ manager (a:as) ctx flag =
               Right s -> 
                 -- manageInputFeed s as ctx flag
                 if' (code == 84) (b (sP res) 84) (if' (as == []) (pSL (sP res) >> m as ct flag) (m as ct flag))
-                where (code, res, ct) = evalSch (stripChars "\t\n\r" $ s) ctx
+                where (code, res, ct) = evalSchTwo (stripChars "\t\n\r" $ s) ctx
